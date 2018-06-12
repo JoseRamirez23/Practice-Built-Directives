d725835988cabd869c98eb38977f7bb40e9c3af9
Jose Ramirez <shadow661199@gmail.com>
2018-06-12 15:54:27 -0700

    practicing built in directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+class Button{
 
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+
+  show :boolean = false;
+
+  arr:any=[ ];
+  time:string='';
+  state:string='';
+
+  displayText(){
+
+    if(this.show === false){
+      this.show = true;
+    }else{
+      this.show = false;
+
+    }
+    this.pushArr();
+    console.log(this.displayText);
+  }
+
+  pushArr(){
+    this.arr.push({
+
+      this:Date(),
+      state:this.show
+    });
+
+    
+  }
+
+ 
 }
